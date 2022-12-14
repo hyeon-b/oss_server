d214c0fbcd0720b539ad7f2a50d4824c24970928
hyeon-b <20201009@sungshin.ac.kr>
2022-12-14 21:30:56 +0900

    #4 style:hp, helper api 명칭 통합

--- src/routers/helper.router.js
+++ src/routers/helper.router.js
@@ -13,10 +13,10 @@ const helperRouter = (router) => {
     router.get('/helper/:hp_id',this.HelperController.getMyApply);
 
     //나의 지원 목록) 메모 보기
-    router.get('/hp_memo/:hp_id/:apply_id',this.HelperController.memoCont);
+    router.get('/helper_memo/:hp_id/:apply_id',this.HelperController.memoCont);
 
     //나의 지원 목록) 메모 수정하기
-    router.post('/hp_memo/:hp_id/:apply_id',this.HelperController.memoUpCont);
+    router.post('/helper_memo/:hp_id/:apply_id',this.HelperController.memoUpCont);
     
 }
 
--- src/routers/hp.apply.router.js
+++ src/routers/hp.apply.router.js
@@ -5,16 +5,16 @@ const hpApplyRouter = (router)=>{
     this.HpApplyController = new HpApplyController();
 
     // 헬퍼 퀵/사전 활동 지원 서비스) 지원하기 -기존 자기소개서 보기
-    router.get('/hp/apply/:hp_id', this.HpApplyController.getHpApply);
+    router.get('/helper/apply/:hp_id', this.HpApplyController.getHpApply);
 
     // 헬퍼 지원 완료하기 -> 지원한 신청서의 시간까지 post
-    router.post('/hp/apply',this.HpApplyController.postHpApply);
+    router.post('/helper/apply',this.HpApplyController.postHpApply);
 
     // 헬퍼 마이페이지) 기존 자기소개서 가져오기
-    router.get('/hp/preidc/:hp_id',this.HpApplyController.getHpPreIdc);
+    router.get('/helper/preidc/:hp_id',this.HpApplyController.getHpPreIdc);
 
     // 헬퍼 마이페이지) 기존 자기소개서 insert or update
-    router.post('/hp/preidc',this.HpApplyController.postHpPreIdc);
+    router.post('/helper/preidc',this.HpApplyController.postHpPreIdc);
 
 }
 
